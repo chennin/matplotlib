e7675500d72d1fa603e530c0785b188783be2016
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-01-05 15:08:49 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -e
 
 # install dependencies
 apk add --no-cache gcc g++ musl-dev make
-pip install --extra-index-url https://alpine-wheels.github.io/index numpy
 
 # build
-pip wheel --no-deps --requirement requirements.txt
+pip wheel --extra-index-url https://alpine-wheels.github.io/index --no-deps --requirement requirements.txt
