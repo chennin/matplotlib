f4c30223bf3985aa3de6a1ed01db8d244b655ab9
Chris Henning <saturos@gmail.com>
2023-01-05 15:22:26 -0500

    Add matplotlib

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache gcc g++ musl-dev make
+pip install --extra-index-url https://alpine-wheels.github.io/index numpy
 
 # build
 pip wheel --no-deps --requirement requirements.txt
